dbf9ae0f34ac5ca5f5232753f327fb0110a861d0
bxxham <mwirigibeckham@gmail.com>
2025-07-20 13:02:54 +0300

    initial commit for arrays 1.c

---
+++ arrays 1.c
@@ -0,0 +1,17 @@
+// BSE-05-0132/2024
+// BECKHAM MWIRIGI
+//GROUP 2
+
+#include <stdio.h>
+
+int main() {
+    int Arr[5] = {19, 29, 18, 71, 11};
+
+    printf("Array elements are:\n");
+    
+    for (int i = 0; i < 5; i++) {
+        printf("%d ", Arr[i]);
+    }
+
+    return 0;
+}
